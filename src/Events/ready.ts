678226f02a17d781e4d2b99b1dd93ac8c386ce19
Ty Mason <tyger796@outlook.com>
2021-11-08 23:40:04 +0000

    Bug fixes
    - Moved the script to connect to the database here instead of in the extended client.

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,8 +1,17 @@
 import { Event } from '../Interfaces/Event';
+import mongoose from 'mongoose';
 
 export const event: Event = {
     name: 'ready',
-    run: (client) => {
+    run: async (client) => {
         console.log(`${client.user.tag} is online!`);
+        client.user.setActivity(`${client.config.prefix}help | ${client.guilds.cache.size} servers`, { type: 'WATCHING' });
+
+        // Connect to AtlasDB
+        await mongoose.connect(client.config.mongoURI).then(() => {
+            console.log('Connected to AtlasDB Successfully!');
+        }).catch((err) => {
+            console.log('Failed to connect to AtlasDB! ' + err);
+        });
     }
 }
